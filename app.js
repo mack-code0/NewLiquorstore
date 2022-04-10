6befe6c522992893f820c2de3cb1671d5b72125e
mack-code0 <macaulayhilbert202@gmail.com>
2022-04-10 12:38:07 -0700

    added helmet and compression

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const csrf = require("csurf")
 const MongoDbStore = require("connect-mongodb-session")(session)
 const MONGODB_URI = process.env.DB_URI
 
+const helmet = require('helmet')
+const compression = require('compression')
+
 
 const app = express()
+app.use(helmet())
+app.use(compression())
+
 const Store = new MongoDbStore({
     uri: MONGODB_URI,
     collection: "sessions",
